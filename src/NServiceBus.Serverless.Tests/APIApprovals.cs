eb53dfc3b18d76e574f09542983f994e5a960d45
David Boike <david.boike@gmail.com>
2019-08-22 12:40:42 -0500

    Add tests project, remove (but don't delete) things moving to AzureFunctions repo

---
+++ src/NServiceBus.Serverless.Tests/APIApprovals.cs
@@ -0,0 +1,17 @@
+﻿namespace NServiceBus.Serverless.Tests
+{
+    using NUnit.Framework;
+    using Particular.Approvals;
+    using PublicApiGenerator;
+
+    [TestFixture]
+    public class APIApprovals
+    {
+        [Test]
+        public void Approve()
+        {
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(ServerlessEndpoint).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+            Approver.Verify(publicApi);
+        }
+    }
+}
